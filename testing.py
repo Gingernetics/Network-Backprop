dfeb1a7b7c8374fdd7399298cf675c562a25cc6f
Gingernetics <janstefankowalski@gmail.com>
2018-04-03 17:45:41 -0400

    refactor

--- testing.py
+++ testing.py
@@ -5,20 +5,35 @@ from random import *
 
 generatedinputs = []
 expectedoutputs = []
-producedoutputs = []
-
-errors = []
 
 net = Network([1, 3, 1])
 
 def test(num_tests):
+    populate_tests(num_tests)
+    return check_network(num_tests)
+
+def populate_tests(num_tests):
     global generatedinputs
     global expectedoutputs
-    global producedoutputs
-    global errors
+
+    for i in range(num_tests):
+        rand_input = randint(-20, 20)
+        if (rand_input >= 0):
+            expected = 1
+        else:
+            expected = 0
+
+        generatedinputs.append([rand_input])
+
+        expectedoutputs.append([expected])
+
+def check_network(num_tests):
+    global generatedinputs
+    global expectedoutputs
     global net
 
-    populate_tests(num_tests)
+    producedoutputs = []
+    errors = []
 
     for i in range(num_tests):
         output = net.getOutput(generatedinputs[i])
@@ -39,21 +54,6 @@ def test(num_tests):
     print producedoutputs
     print "Stuff Correct: " + str(1 - len(errors)*1.0/num_tests)
     return errors
-
-def populate_tests(num_tests):
-    global generatedinputs
-    global expectedoutputs
-
-    for i in range(num_tests):
-        rand_input = randint(-20, 20)
-        if (rand_input >= 0):
-            expected = 1
-        else:
-            expected = 0
-
-        generatedinputs.append([rand_input])
-
-        expectedoutputs.append([expected])
 
 def backprop():
     pass
